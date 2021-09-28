0c963de8644d1d828ccc2c2cffe90224454c3705
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-28 02:50:21 -0600

    packages installments

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,67 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text, Button, VStack } from '@chakra-ui/react'
 import useDelay from '../hooks/useDelay'
 import { motion } from 'framer-motion'
-
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Index'
+import Head from 'next/head'
+import { useRef } from 'react'
 const MotionFlex = motion(Flex)
 export default function Home() {
   const delay = useDelay(2000)
+  const innerColor = '151c49'
+  const outerColor = '000001'
+  const streamRef = useRef()
+  const playStream = () => {
+    streamRef.current.volume = 0.2
+    streamRef.current.play()
+  }
   return (
     <>
+      <Head>
+        <title>Radio One</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       {delay ? (
         'Loading...'
       ) : (
-        <MotionFlex
-          w="full"
-          h="100vh"
-          bg="red"
-          justify="center"
-          align="center"
-          overflow="hidden"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          <Flex
-            position="absolute"
+        <>
+          <Navbar />
+          <MotionFlex
             w="full"
             h="100vh"
-            zIndex="4"
-            opacity=".9"
-            bgGradient="radial(#151c49 0%, #000000 90%)"
-          ></Flex>
-          <video width="100%" autoPlay="autoplay" loop="loop" muted>
-            <source src="/video/waves.mp4" />
-          </video>
-        </MotionFlex>
+            bg="blue"
+            justify="center"
+            align="center"
+            overflow="hidden"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <VStack
+              position="absolute"
+              justify="center"
+              align="center"
+              w="full"
+              h="100vh"
+              zIndex="6"
+              opacity=".9"
+              bgGradient={`radial(#${innerColor} 0%, #${outerColor} 90%)`}
+            >
+              <Text color="#fee" fontFamily="Orbitron" fontWeight="900">
+                Estamos en vivo
+              </Text>
+              <Button onClick={playStream}>Escuchar</Button>
+            </VStack>
+            <Flex position="absolute" w="full" h="100vh" zIndex="5"></Flex>
+            <video className="videoStyle" autoPlay="autoplay" loop="loop" muted>
+              <source src="/video/waves.mp4" />
+            </video>
+            <audio ref={streamRef} src="https://lfhh.radioca.st/stream"></audio>
+          </MotionFlex>
+          <Footer />
+        </>
       )}
     </>
   )
